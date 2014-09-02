8acfed6a5961f27fe05a5a6d447fd8c423499ced
MB <fesnandu@live.it>
2014-09-02 09:20:43 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,36 +1,13 @@
 (function () {
 
-    //Define our function responsible for extending the bot.
     function extend() {
-        //If the bot hasn't been loaded properly, try again in 1 second(s).
-        if (!window.bot) {
+            if (!window.bot) {
             return setTimeout(extend, 1 * 1000);
         }
 
-        //Precaution to make sure it is assigned properly.
         var bot = window.bot;
 
-        //Load custom settings set below
         bot.retrieveSettings();
-
-        /*
-         Extend the bot here, either by calling another function or here directly.
-         Model code for a bot command:
-
-         bot.commands.commandCommand = {
-         command: 'cmd',
-         rank: 'user/bouncer/mod/manager',
-         type: 'startsWith/exact',
-         functionality: function(chat, cmd){
-         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-         if( !bot.commands.executable(this.rank, chat) ) return void (0);
-         else{
-         //Commands functionality goes here.
-         }
-         }
-         }
-
-         */
 
         bot.commands.baconCommand = {
             command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
@@ -44,19 +21,36 @@
                 }
             }
         };
+        
+        bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
+                    'deu-lhe um biscoito de aveia caseiro macio!',
+                    'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
+                    'deu-lhe um bolinho de açúcar... O quê? Sem estrelinhas e povilho? Eu não tocaria.',
+                    'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
+                    'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... estranho',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Por que você não está trabalhando?"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Cumprimente agora a pessoa que você ama"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Arrisque-se!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Não esqueça de comer os vegetais"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão te achar sexy!',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo"',
+                    'deu-lhe um biscoito de ouro, mas não dá pra comer... Droga!',
+                    'deu-lhe um Oreo e um copo de leite.',
+                    'deu-lhe um biscoito de arco-íris feito com amor :heart:',
+                    'deu-lhe um biscoito que fio esquecido na chuva... eu não comeria.',
+                    'te trouxe biscoitos fresquinhos... parecem deliciosos!'
+                ];
 
-        //Load the chat package again to account for any changes
         bot.loadChat();
-
     }
 
-    //Change the bots default settings and make sure they are loaded on launch
-
+    
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
-        language: "english",
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-        maximumAfk: 120,
+        botName: "FarofinhaBot",
+        language: "portuguese",
+        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
+        maximumAfk: 60,
         afkRemoval: true,
         maximumDc: 60,
         bouncerPlus: true,
@@ -72,35 +66,34 @@
         usercommandsEnabled: true,
         lockskipPosition: 3,
         lockskipReasons: [
-            ["theme", "This song does not fit the room theme. "],
-            ["op", "This song is on the OP list. "],
-            ["history", "This song is in the history. "],
-            ["mix", "You played a mix, which is against the rules. "],
-            ["sound", "The song you played had bad sound quality or no sound. "],
-            ["nsfw", "The song you contained was NSFW (image or sound). "],
-            ["unavailable", "The song you played was not available for some users. "]
-        ],
+                ["tema", "A música não se encaixa nos padrões da sala. "],
+                ["op", "Essa música está na lista OP. "],
+                ["historico", "A música ainda está no histórico. "],
+                ["mix", "Você tocou um mix (muito longo) - não permitido. "],
+                ["som", "A música que você tocou tinha má qualidade ou estava sem som. "],
+                ["nsfw", "A música que você tocou é NSFW (impróprio). "],
+                ["indisponivel", "A música que você tocou está indisponível. "]
+            ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
         motdEnabled: false,
-        motdInterval: 5,
-        motd: "Temporary Message of the Day",
+        motdInterval: 8,
+        motd: "!roulette",
         filterChat: true,
         etaRestriction: false,
-        welcome: true,
+        welcome: false,
         opLink: null,
         rulesLink: null,
         themeLink: null,
-        fbLink: null,
+        fbLink: "https://www.facebook.com/DivasdaDepressao",
         youtubeLink: null,
-        website: null,
+        website: "https://www.facebook.com/DivasdaDepressao",
         intervalMessages: [],
         messageInterval: 5,
-        songstats: true,
+        songstats: false,
         commandLiteral: "!"
     }));
 
-    //Start the bot and extend it when it has loaded.
     $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
 
 }).call(this);
