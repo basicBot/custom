bab78889578236e93b7b3f94d23859b7673cfc66
Booster123 <jgodinho123@gmail.com>
2014-10-16 12:55:12 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -54,8 +54,8 @@
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: "basicBot",
-        language: "english",
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
+        language: "portuguese",
+        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
         maximumAfk: 120,
         afkRemoval: true,
         maximumDc: 60,
