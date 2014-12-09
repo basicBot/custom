99eb658901da8364ac22edc3bfd805e6a4d7761f
LetoWelter <welington_welter@outlook.com>
2014-12-09 19:27:50 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -9,6 +9,19 @@
 
         bot.retrieveSettings();
         
+        bot.commands.baconCommand = {
+            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Bacon!! :pig2:");
+                }
+            }
+        };
+        
         bot.commands.origemCommand = {
             command: 'origem',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
