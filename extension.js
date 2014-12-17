73e2861adf4a36b7e01c7f7f312ec27e4ead0cc3
Kaueterror <kauegames9@gmail.com>
2014-12-17 11:20:43 -0200

    Update extension.js
    Fezzes are cool

--- extension.js
+++ extension.js
@@ -33,14 +33,14 @@
          */
 
         bot.commands.baconCommand = {
-            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+            command: 'fez',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon!!!");
+                    API.sendChat("/me Fezzes are cool");
                 }
             }
         };
