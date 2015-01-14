9161f145074a543db83723642b8cda77d4c3fd97
audiomoddified <audiomodder@gmail.com>
2015-01-13 20:09:53 -0600

    Added Scotch command

--- extension.js
+++ extension.js
@@ -45,6 +45,19 @@
             }
         };
 
+		bot.commands.baconCommand = {
+            command: 'scotch',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me gives audiomoddified a Glennfiddich 12 year, he deserves it");
+                }
+            }
+        };
+		
         //Load the chat package again to account for any changes
         bot.loadChat();
 
