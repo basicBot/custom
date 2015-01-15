bb34d4adbadf5744ad4b0a7c945f187d44b65263
RatedRion <ratedrion@gmail.com>
2015-01-15 16:13:52 -0500

    fixed spacing

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 (function () {
-		//Link location of your fork so you don't have to modify so many things.
-		var fork = "Yemasthui";
+    //Link location of your fork so you don't have to modify so many things.
+    var fork = "Yemasthui";
 		
     //Define our function responsible for extending the bot.
     function extend() {
