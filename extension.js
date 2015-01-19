2b83dcb58443c9d2c877932864fb8711445d643f
LetoWelter <welington_welter@outlook.com>
2015-01-19 05:14:33 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -22,6 +22,19 @@
             }
         };
         
+        bot.commands.aquelacarinhaCommand = {
+            command: 'aquelacarinha',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat(" [@" + chat.un + "] ( ͡° ͜ʖ ͡°)");
+                }
+            }
+        };
+        
         bot.commands.origemCommand = {
             command: 'origem',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
