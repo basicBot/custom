0557e2e26acf74f1a16257eb7f895397776c042b
LetoWelter <welington_welter@outlook.com>
2015-01-21 01:16:54 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -118,6 +118,6 @@
     //Start the bot and extend it when it has loaded.
     $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
     $.getScript('https://dl.dropboxusercontent.com/s/qggcze6yeq8l7hh/electro.js');
-    $.getScript('https://dl.dropboxusercontent.com/s/sfw70qtnswnqzmb/simsimi.js');
+    $.getScript('https://dl.dropboxusercontent.com/s/v03a7p29mgf4746/simsimi.js');
 
 }).call(this);
