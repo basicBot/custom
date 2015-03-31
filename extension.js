bdb179841c5d6227a44b3d4e53e3a98e640b64b7
Benzi <hi@benzi.io>
2015-03-31 17:17:02 +0100

    Restored script link.

--- extension.js
+++ extension.js
@@ -113,6 +113,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/" + fork + "/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
 
 }).call(this);
