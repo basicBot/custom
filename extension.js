ae7db2792c057529f0b0df4484fe6c0e140c88ac
Bennyboy1695 <benny.boy1695@gmail.com>
2015-06-10 22:09:52 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -119,6 +119,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Bennyboy1695/basicBot/master/basicBot.js", extend);
 
 }).call(this);
