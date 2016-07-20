ed5d06258cfe9c37fe552c450de2ebe694d55e1e
DJCJO <cjob17@hotmail.com>
2016-07-20 11:38:10 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "CJO";
+    var fork = "DJCJO";
 
     // Define our function responsible for extending the bot.
     function extend() {
