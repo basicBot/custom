88a760c6ad81a8b8722199c3e070c3fd370eb56b
HarryMcKenzie <42568513+HarryMcKenzie@users.noreply.github.com>
2018-08-21 09:44:14 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "basicBot";
+    var fork = "HarryMcKenzie";
 
     // Define our function responsible for extending the bot.
     function extend() {
